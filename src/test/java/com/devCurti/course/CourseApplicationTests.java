ac0e884668ef3eac5dd251c2b18f8730f758b06d
devCurti <joaocurti2002@gmail.com>
2023-07-27 12:10:37 -0300

    Initial commit

---
+++ src/test/java/com/devCurti/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devCurti.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
