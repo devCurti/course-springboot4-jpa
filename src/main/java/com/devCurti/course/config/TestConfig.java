8f2f6e54c26e8cff91285e5dfb222a53824894bc
devCurti <joaocurti2002@gmail.com>
2023-07-31 11:01:34 -0300

    Added OrderStatus enum

--- src/main/java/com/devCurti/course/config/TestConfig.java
+++ src/main/java/com/devCurti/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.devCurti.course.entities.Order;
 import com.devCurti.course.entities.User;
+import com.devCurti.course.entities.enums.OrderStatus;
 import com.devCurti.course.repositories.OrderRepository;
 import com.devCurti.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User("Maria Brown", "maria@gmail.com", "999999", "12345");
 		User u2 = new User("Carlos Brown", "carlos@gmail.com", "999999", "12345");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-08-20T17:51:00Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-20T20:20:05Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.SHIPPED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-08-20T17:51:00Z"), OrderStatus.DELIVERED,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-20T20:20:05Z"), OrderStatus.SHIPPED,u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
